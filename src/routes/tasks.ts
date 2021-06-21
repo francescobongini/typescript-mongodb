c3129f88e44798ce1ced1db2b2ae9a8ce41e49f8
francescobongini <bongini.francesco@gmail.com>
2021-06-21 10:49:05 +0100

    Update

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import * as readline from 'readline';
 const router = Router();
 
 // Model
@@ -9,11 +10,27 @@ router.route('/create')
         res.render('tasks/create');
     })
     .post(async (req: Request, res: Response) => {
-        const { title, description, available } = req.body;
-        const task = new Task({ title, description,available});
+        let rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+          });
+          
+          rl.question('Inserire password: ', async (answer) => {
+            switch(answer.toLowerCase()) {
+              case '0000':
+                console.log('Super!');
+                const { title, description, available } = req.body;
+                const task = new Task({ title, description,available});
+                await task.save();
+                res.redirect('/tasks/list');
+                break;
+              default:
+                console.log('Invalid answer!');
+            }
+            rl.close();
+          });
+        
 
-        await task.save();
-        res.redirect('/tasks/list');
     });
 
 
@@ -35,9 +52,27 @@ router.route('/occupato')
 
 router.route('/delete/:id')
     .get(async (req: Request, res: Response) => {
-        const { id } = req.params;
-        await Task.findByIdAndDelete(id);
-        res.redirect('/tasks/list');
+
+
+        let rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        });
+        
+        rl.question('Inserire password: ', async (answer) => {
+          switch(answer.toLowerCase()) {
+            case '0000':
+              console.log('Super!');
+              const { id } = req.params;
+              await Task.findByIdAndDelete(id);
+              res.redirect('/tasks/list');
+              break;
+            default:
+              console.log('Invalid answer!');
+          }
+          rl.close();
+        });
+
     });
 
 
@@ -46,7 +81,7 @@ router.route('/edit/:id')
     .get(async (req: Request, res: Response) => {
         const { id } = req.params;
         const task = await Task.findById(id);
-        console.log(task)
+        //console.log(task)
         if(task['available']=="prenotato"){
             console.log("lo slot non è più disponibile!")
             //res.redirect('/tasks/list2');
@@ -71,6 +106,7 @@ router.route('/edit/:id')
                 $set: {  "available" : "prenotato"}
             }
         )
+        console.log(task);
         res.redirect('/tasks/list');
         }
 )
